ba954e6cc6114d989e92d61137ee026da6feb592
AndreaFlorencia <andreabejarano629@gmail.com>
2021-05-11 00:00:32 -0300

    task:Agregue el arrayList de compra Autor:Andrea Bejarano

---
+++ GrupoPV_TP5/src/main/java/ar/edu/unju/fi/tp5/util/TablaCompra.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.tp5.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import ar.edu.unju.fi.tp5.model.Compra;
+
+public class TablaCompra {
+
+	public static List<Compra> compras = new ArrayList<>(); 
+}
